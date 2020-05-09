596a594aa2bae9bb134b89743a152b45624526ed
Aatira <aatira.b@attinadsoftware.com>
2020-05-09 07:16:46 +0530

    Auto-generated commit

---
+++ task-app/5-es2015.e08307e555f430a6f42f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"BJ+2":function(n,o,s){"use strict";s.r(o),s.d(o,"FormModule",(function(){return c}));class c{}}}]);
---
+++ task-app/6-es2015.a184d83153951ed4d3e6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"7UCR":function(n,s,o){"use strict";o.r(s),o.d(s,"UserModule",(function(){return c}));class c{}}}]);
